20d737ffc33d8c44392ba188dd982837d062366d
ktshun <ktshun1027@gmail.com>
2020-04-22 15:25:50 +0900

    remove unused code

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from flask import Flask
 import app
 
-##app = Flask(__name__)
-
 if __name__ == "__main__":
-    ##app.register_blueprint(SlackController.app, url_prefix = '/slack')
     app = app.create_app()
     app.run(debug=app.config['DEBUG'], host=app.config['HOST'], port=app.config['PORT'])
